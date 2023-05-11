22960a5d78f9bf418138623f4f870ad3e8dfe977
Abdulaziz <lchoriyev00@gmail.com>
2023-05-11 16:22:31 +0500

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+
+tailwind.config.js
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
